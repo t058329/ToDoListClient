0183af34c661effdbe5f18eed3d0f1edbc8b9911
t058329 <t0583293049@gmail.com>
2023-04-17 19:04:50 +0300

    my commit

---
+++ src/service.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5041';
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/items`)    
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    const result = await axios.post(`/items`,{name:name,isComplete:false})    
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    await axios.put(`/items/${id}?isComplete=${isComplete}`)
+  },
+
+  deleteTask:async(id)=>{
+    const result = await axios.delete(`/items/${id}`)    
+    return result.data;
+  }
+};
+
+axios.interceptors.response.use(function (response) {
+  
+  return response;
+}, function (error) {
+  console.error(error);
+  return Promise.reject(error);
+});
+
+
