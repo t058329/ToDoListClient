f937562e39ced55c22632a87f7ca536d71a4f18f
t058329 <117013416+t058329@users.noreply.github.com>
2023-05-14 00:47:53 +0300

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,34 +1,35 @@
 import axios from 'axios';
 
 
- axios.defaults.baseURL = 'http://localhost:5041';
+//  axios.defaults.baseURL = 'http://localhost:5041';
 // axios.defaults.baseURL=process.env.REACT_APP_API;
-// const apiClient=axios.create({
-//   baseURL:process.env.REACT_APP_API
-// })
+const apiClient=axios.create({
+  baseURL:process.env.REACT_APP_API
+})
+console.log('process.env.API_URL', process.env.REACT_APP_API)
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`/items`)    
+    const result = await apiClient.get(`/items`)    
     return result.data;
   },
 
   addTask: async(name)=>{
-    const result = await axios.post(`/items`,{name:name,isComplete:false})    
+    const result = await apiClient.post(`/items`,{name:name,isComplete:false})    
     return result.data;
   },
 
   setCompleted: async (id, isComplete) => {
-    await axios.put(`/items/${id}?isComplete=${isComplete}`)
+    await apiClient.put(`/items/${id}?isComplete=${isComplete}`)
   },
 
   deleteTask:async(id)=>{
-    const result = await axios.delete(`/items/${id}`)    
+    const result = await apiClient.delete(`/items/${id}`)    
     return result.data;
   }
 };
 
-axios.interceptors.response.use(function (response) {
+apiClient.interceptors.response.use(function (response) {
   
   return response;
 }, function (error) {
